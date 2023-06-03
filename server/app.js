55236137342d876f03138974a9e08acbd2a89387
LiorNakar <nakarlior@gmail.com>
2023-06-03 20:47:13 +0300

    starting sockets

--- server/app.js
+++ server/app.js
@@ -1,19 +1,67 @@
-const express = require('express'); 
-var app = express();
+// todo before
+// const express = require('express'); 
+// var app = express();
 
+// const bodyParser = require('body-parser'); 
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// const cors = require('cors'); 
+// app.use(cors());
+
+// const customEnv = require('custom-env'); 
+// customEnv.env(process.env.NODE_ENV, './config'); 
+// console.log(process.env.CONNECTION_STRING)
+// console.log(process.env.PORT)
+
+// const mongoose = require('mongoose')
+// mongoose.connect(process.env.CONNECTION_STRING, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+
+// app.use(express.static('public'))
+
+// const Chats = require('./routes/Chats')
+// app.use('/api/Chats', Chats);
+// const Users = require('./routes/Users')
+// app.use('/api/Users', Users);
+// const Tokens = require('./routes/Tokens')
+// app.use('/api/Tokens', Tokens);
+
+// app.listen(process.env.PORT)
+
+// todo after
+
+const express = require('express'); 
 const bodyParser = require('body-parser'); 
+var app = express();
+const http = require('http')
+const cors = require('cors'); 
+const customEnv = require('custom-env'); 
+const mongoose = require('mongoose')
+
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-const cors = require('cors'); 
 app.use(cors());
 
-const customEnv = require('custom-env'); 
 customEnv.env(process.env.NODE_ENV, './config'); 
 console.log(process.env.CONNECTION_STRING)
 console.log(process.env.PORT)
 
-const mongoose = require('mongoose')
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ["GET", "POST", "DELETE"],
+        credentials: true
+    }
+})
+
+
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -28,4 +76,21 @@ app.use('/api/Users', Users);
 const Tokens = require('./routes/Tokens')
 app.use('/api/Tokens', Tokens);
 
-app.listen(process.env.PORT)
+// io.use(cors())
+
+io.on('connection', (socket) => {
+    console.log("the socket id is", socket.id)
+    socket.on('msg', (msg)=>{
+        console.log(msg)
+    })
+    socket.on('disconnect', ()=>{
+        console.log('disconntected')
+    })
+})
+
+server.listen(process.env.PORT,()=>{
+
+    console.log(`app is listening on poort ${process.env.PORT}`);
+})
+
+// app.listen(process.env.PORT)
---
+++ whatsapp/src/sockets/socket.js
@@ -0,0 +1,12 @@
+import io from "socket.io-client"
+const socket = io("http://localhost:12345")
+socket.emit('msg', 'asaaaaaaaaaa')
+
+
+socket.on("connect", () => {
+    console.log("connected");
+  });
+  
+  socket.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
